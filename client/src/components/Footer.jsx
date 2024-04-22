82c4acf4fb7269fc75dca514b5b76662ff6765a4
Rajat Pal <rajatpalbnk@gmail.com>
2024-04-22 15:39:53 +0530

    sigup functionality and create footer UI

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,99 @@
+
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { IoLogoFacebook } from "react-icons/io";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+
+export default function FooterCom() {
+  return (
+  <Footer className=' border border-t-8 border-teal-500 py-4 px-4'>
+    <div className=" w-full max-w-7xl mx-auto">
+        <div className=" grid w-full justify-between sm:flex md:grid-cols-1">
+            <div className=" mt-5">
+                <Link to="/" className=" self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+                <span className="  py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Max's</span>
+                Blog
+                </Link>
+            </div>
+            <div className=" grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+                <div>
+                <Footer.Title title='About' />
+                <Footer.LinkGroup col>
+                  <Footer.Link
+                    href='https://chat-app-koj0.onrender.com'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Chat App
+                  </Footer.Link>
+                  <Footer.Link
+                    href='/about'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Max's Blog
+                  </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                <div>
+                <Footer.Title title='Follow us' />
+                <Footer.LinkGroup col>
+                  <Footer.Link
+                    href='https://github.com/rajatpal47'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Github
+                  </Footer.Link>
+                  <Footer.Link
+                    href='#'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Discord
+                  </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                <div>
+                <Footer.Title title='Legal' />
+                <Footer.LinkGroup col>
+                  <Footer.Link
+                    href='#'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Privacy policy
+                  </Footer.Link>
+                  <Footer.Link
+                    href='#'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Terms &amp; Conditions
+                  </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+            </div>
+        </div>
+        <Footer.Divider />
+        <div className=" w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright 
+          href='#'
+          by="Max's Blog"
+          year={new Date().getFullYear()} 
+          />
+          <div className=" flex gap-5 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={IoLogoFacebook} />
+            <Footer.Icon href='#' icon={IoLogoInstagram} />
+            <Footer.Icon href='#' icon={FaXTwitter} />
+            <Footer.Icon href='#' icon={FaLinkedin} />
+            <Footer.Icon href='#' icon={FaGithub} />
+          </div>
+        </div>
+    </div>
+  </Footer>
+)
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Signup() {
+
+    const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+      setFormData({...formData, [e.target.id]: e.target.value.trim()});
+    }
+    
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      if(!formData.username || !formData.email || !formData.password){
+        return setErrorMessage("please fill all fields");
+      }
+
+      try {
+        setLoading(true);
+        setErrorMessage(null);
+        const res = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if(data.success === false) {
+          return setErrorMessage(data.message);
+        }
+        setLoading(false);
+        if(res.ok){
+          navigate('/signin');
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+        setLoading(false);
+      }
+    };
+
   return (
-    <div>Signup</div>
+    <div className=" min-h-screen mt-20">
+      <div className=" flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-8">
+        {/* left side  */}
+        <div className=" flex-1">
+          <Link 
+          to="/" 
+          className=" font-bold dark:text-white text-4xl"
+          >
+            <span className=" px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500     to-pink-500 rounded-lg text-white">Max's</span>
+            Blog
+          </Link>
+          <p className=" text-sm mt-5">
+            Join us today to access exclusive features and connect with like-minded   individuals. Start your journey with just a few simple steps!
+          </p>
+        </div>
+        {/* right side  */}
+        <div className=" flex-1">
+          <form className=" flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your username"/>
+              <TextInput type="text" placeholder="max_name" id="username" onChange={handleChange}/>
+            </div>
+            <div>
+              <Label value="Your email"/>
+              <TextInput type="email" placeholder="max@gmail.com" id="email" onChange={handleChange}/>
+            </div>
+            <div>
+              <Label value="Your password"/>
+              <TextInput type="password" placeholder="password" id="password" onChange={handleChange}/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className="pl-3">Loading...</span>
+                  </>
+                ) : "Sign Up"
+              }
+            </Button>
+          </form>
+          <p className=" text-sm mt-5">
+            Already have an account? <Link to="/signin" className=" text-blue-600">Sign In</Link>
+          </p>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+        
+      </div>
+    </div>
   )
 }

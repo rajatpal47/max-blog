7a98c994fe9cfa1a8e160b667d0700f91573ed51
Rajat Pal <rajatpalbnk@gmail.com>
2024-04-25 00:44:38 +0530

    add user profilein header and make dashboard private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,16 @@
-import { Button, Navbar, TextInput} from "flowbite-react"
+import { Avatar, Button, Dropdown, Navbar, TextInput} from "flowbite-react"
 import { Link, useLocation } from 'react-router-dom'
 import { IoSearch } from "react-icons/io5";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from "../redux/theme/themeSlice";
+
 
 export default function Header() {
   const path = useLocation().pathname;
+  const {currentUser} = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const {theme} = useSelector((state) => state.theme)
   return (
      <Navbar className='border-b-2'>
       <Link to="/" className=" self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
@@ -23,14 +29,40 @@ export default function Header() {
         <IoSearch/>
       </Button>
       <div className=" flex gap-2 md:order-2">
-        <Button className=" w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button className=" w-12 h-10 hidden sm:inline" color="gray" pill onClick={() => dispatch(toggleTheme())}>
+          {theme === 'light' ? <FaSun/> : <FaMoon/>}
         </Button>
-        <Link to='/signin'>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon = {false}
+            inline
+            label = {
+              <Avatar
+                alt="user"
+                img={currentUser.profilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className=" block text-sm">@{currentUser.username}</span>
+              <span className=" block text-sm font-medium truncate">{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>
+              Sign out
+            </Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/signin'>
           <Button outline gradientDuoTone='purpleToBlue'>
             Sign In
           </Button>
         </Link>
+        )}
         <Navbar.Toggle/>
       </div>
       <Navbar.Collapse>

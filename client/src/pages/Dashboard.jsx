659d006cefd0e4a8a2b6f0af3d999b8d836f38da
Rajat Pal <rajatpalbnk@gmail.com>
2024-04-25 02:02:08 +0530

    create dashboard Sidebar and Porfile Section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashboardSidebar from "../components/DashboardSidebar"
+import DashboardMain from "../components/DashboardMain"
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search])
+  return ( 
+  <div className=" min-h-screen flex flex-col md:flex-row">
+    <div className=" md:w-56">
+      <DashboardSidebar />
+    </div>
+    {tab === 'profile' && <DashboardMain />}
+  </div>
   )
 }
